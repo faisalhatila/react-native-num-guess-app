2356413992e7a91fa43db194b688a76bb0e25733
faisalhatila <hatilas1994@gmail.com>
2020-08-12 08:42:35 -0700

    input component created

---
+++ components/input/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const Input = () => {
+  return <TextInput />;
+};
+const styles = StyleSheet.create({});
+export default Input;
