fd2aff1f7cab5decf9ac39ffa01eb1f810e3baf2
faisalhatila <hatilas1994@gmail.com>
2020-08-12 07:34:26 -0700

    start game screen made

--- screens/startgamescreen/index.js
+++ screens/startgamescreen/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const StartGameScreen = () => {};
+const StartGameScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game Screen!</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    // justifyContent:"center"
+  },
+});
 
 export default StartGameScreen;
