c5cd74ca427c88d89ee14bfffd4d73ee30606d0a
faisalhatila <hatilas1994@gmail.com>
2020-08-12 08:51:17 -0700

    input component fixed

--- components/input/index.js
+++ components/input/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { TextInput, StyleSheet } from "react-native";
 
-const Input = () => {
+const Input = (props) => {
   return <TextInput style={{ ...styles.input, ...props.style }} />;
 };
 const styles = StyleSheet.create({
