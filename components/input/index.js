23e3b44dede3ebf98624b44ba2ec50b085faa637
faisalhatila <hatilas1994@gmail.com>
2020-08-12 08:46:45 -0700

    input component made

--- components/input/index.js
+++ components/input/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import { TextInput, StyleSheet } from "react-native";
 
 const Input = () => {
-  return <TextInput />;
+  return <TextInput style={{ ...styles.input, ...props.style }} />;
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 30,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+});
 export default Input;
