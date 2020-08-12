0e1f470ca9d0d7bf6435d79498a80206e634c60e
faisalhatila <hatilas1994@gmail.com>
2020-08-12 08:13:40 -0700

    card component made

--- components/card/index.js
+++ components/card/index.js
@@ -1,7 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-const Card = () => {
-  return <View></View>;
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.styles }}>{props.children}</View>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "#ccc",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5,
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 6,
+  },
+});
 export default Card;
