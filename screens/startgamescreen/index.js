e37412fd6d8e175c2853d37a9a62027ecea8f6b7
faisalhatila <hatilas1994@gmail.com>
2020-08-12 07:17:44 -0700

    start game screen created

---
+++ screens/startgamescreen/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = () => {};
+
+const styles = StyleSheet.create({});
+
+export default StartGameScreen;
