c9777c609baeb5f173a3cea71caf946d8ff3ebb3
faisalhatila <hatilas1994@gmail.com>
2020-08-12 08:48:22 -0700

    input component exported

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import _Header from "./header";
 import _Card from "./card";
+import _Input from "./input";
 
 export const Header = _Header;
 export const Card = _Card;
+export const Input = _Input;
