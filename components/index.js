c9b0ce92d56f15a1b604272141a0d5cf82fa1d36
faisalhatila <hatilas1994@gmail.com>
2020-08-13 07:49:58 -0700

    title text exported

--- components/index.js
+++ components/index.js
@@ -3,9 +3,11 @@ import _Card from "./card";
 import _Input from "./input";
 import _NumberContainer from "./numbercontainer";
 import _BodyText from "./bodytext";
+import _TitleText from "./titletext";
 
 export const Header = _Header;
 export const Card = _Card;
 export const Input = _Input;
 export const NumberContainer = _NumberContainer;
 export const BodyText = _BodyText;
+export const TitleText = _TitleText;
