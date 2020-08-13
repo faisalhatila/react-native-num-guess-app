0cf1a9a184bd2016433cd21c37fe8f449185165b
faisalhatila <hatilas1994@gmail.com>
2020-08-13 07:46:19 -0700

    body text integerated successfully

--- screens/startgamescreen/index.js
+++ screens/startgamescreen/index.js
@@ -9,7 +9,7 @@ import {
   Keyboard,
   Alert,
 } from "react-native";
-import { Card, Input, NumberContainer } from "../../components";
+import { Card, Input, NumberContainer, BodyText } from "../../components";
 import Colors from "../../constants/colors";
 const StartGameScreen = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -47,7 +47,7 @@ const StartGameScreen = (props) => {
   if (confirmed)
     confirmedOutput = (
       <Card style={styles.summaryContainer}>
-        <Text>You selected: {selectedNumber}</Text>
+        <BodyText>You selected: {selectedNumber}</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
         <Button
           title="START GAME"
@@ -66,7 +66,7 @@ const StartGameScreen = (props) => {
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
