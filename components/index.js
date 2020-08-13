d606c0ddf70e2cb8273e13df3fa693d57f1355cc
faisalhatila <hatilas1994@gmail.com>
2020-08-13 07:37:39 -0700

    body text component exported

--- components/index.js
+++ components/index.js
@@ -2,8 +2,10 @@ import _Header from "./header";
 import _Card from "./card";
 import _Input from "./input";
 import _NumberContainer from "./numbercontainer";
+import _BodyText from "./bodytext";
 
 export const Header = _Header;
 export const Card = _Card;
 export const Input = _Input;
 export const NumberContainer = _NumberContainer;
+export const BodyText = _BodyText;
