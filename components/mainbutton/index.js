7704d3f480d69ac5bde33d868bd60a16729b4577
faisalhatila <hatilas1994@gmail.com>
2020-08-13 09:56:40 -0700

    main button component done

--- components/mainbutton/index.js
+++ components/mainbutton/index.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, TouchableOpacity } from "react-native";
+import Colors from "../../constants/colors";
 
 const MainButton = (props) => {
   return (
-    <View>
-      <Button />
-      <Text></Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        props.onPress;
+      }}
+    >
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.primaryColor,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+  },
+  buttonText: {
+    color: "#fff",
+    fontFamily: "open-sans",
+    fontSize: 18,
+  },
+});
 
 export default MainButton;
