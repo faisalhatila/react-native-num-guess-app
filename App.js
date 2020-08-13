77d8772a441c97306bb1fba87dba0f7af412ac2d
faisalhatila <hatilas1994@gmail.com>
2020-08-13 07:23:40 -0700

    font fetch successfully

--- App.js
+++ App.js
@@ -3,10 +3,28 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./components";
 import { StartGameScreen, GameScreen, GameOverScreen } from "./screens";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   const configureNewGameHanlder = () => {
     setGuessRounds(0);
