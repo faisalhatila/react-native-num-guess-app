2224a83c2a596976bfddca0c41cce277b0f2c1b4
faisalhatila <hatilas1994@gmail.com>
2020-08-13 09:48:21 -0700

    main button component created

---
+++ components/mainbutton/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
+
+const MainButton = (props) => {
+  return (
+    <View>
+      <Button />
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MainButton;
