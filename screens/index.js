524251c6e6620b8d0175f691f428952ab7e9b317
faisalhatila <hatilas1994@gmail.com>
2020-08-13 04:32:41 -0700

    game screen exported

--- screens/index.js
+++ screens/index.js
@@ -1,3 +1,5 @@
 import _StartGameScreen from "./startgamescreen";
+import _GameScreen from "./gamescreen";
 
 export const StartGameScreen = _StartGameScreen;
+export const GameScreen = _GameScreen;
