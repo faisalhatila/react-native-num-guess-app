591e76bf452a257446457d0b5f3ca74bf9acf785
faisalhatila <hatilas1994@gmail.com>
2020-08-13 02:08:55 -0700

    number container component exported

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import _Header from "./header";
 import _Card from "./card";
 import _Input from "./input";
+import _NumberContainer from "./numbercontainer";
 
 export const Header = _Header;
 export const Card = _Card;
 export const Input = _Input;
+export const NumberContainer = _NumberContainer;
