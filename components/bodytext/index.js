36480ee41fe40fc7279f4d90fe8812fa2b384d89
faisalhatila <hatilas1994@gmail.com>
2020-08-13 07:39:30 -0700

    body text component fixed

--- components/bodytext/index.js
+++ components/bodytext/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const BodyText = (props) => {
-  return (
-    <Text style={{ ...props.style, ...styles.body }}>{props.children}</Text>
-  );
+  return <Text style={styles.body}>{props.children}</Text>;
 };
 const styles = StyleSheet.create({
   body: {
