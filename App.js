9a51b6c68b729530bfe6f33374594af17027de48
faisalhatila <hatilas1994@gmail.com>
2020-08-13 04:34:18 -0700

    game screen rendered

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./components";
-import { StartGameScreen } from "./screens";
+import { StartGameScreen, GameScreen } from "./screens";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
       <StartGameScreen />
+      <GameScreen />
     </View>
   );
 }
