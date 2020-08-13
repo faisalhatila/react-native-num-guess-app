395025942f1fea507b3a45b1685d4a74f7511442
faisalhatila <hatilas1994@gmail.com>
2020-08-13 09:49:11 -0700

    main button exported

--- components/index.js
+++ components/index.js
@@ -4,6 +4,7 @@ import _Input from "./input";
 import _NumberContainer from "./numbercontainer";
 import _BodyText from "./bodytext";
 import _TitleText from "./titletext";
+import _MainButton from "./mainbutton";
 
 export const Header = _Header;
 export const Card = _Card;
@@ -11,3 +12,4 @@ export const Input = _Input;
 export const NumberContainer = _NumberContainer;
 export const BodyText = _BodyText;
 export const TitleText = _TitleText;
+export const MainButton = _MainButton;
