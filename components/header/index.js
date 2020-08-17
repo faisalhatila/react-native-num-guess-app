21d46d362535ecf01885c00a54c7df32d05fb8ee
faisalhatila <hatilas1994@gmail.com>
2020-08-17 03:23:05 -0700

    styles changes based on different os , platform api used

--- components/header/index.js
+++ components/header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import Colors from "../../constants/colors";
 // import { TitleText } from "..";
 const Header = (props) => {
@@ -16,9 +16,12 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primaryColor,
+    // backgroundColor: Colors.primaryColor,
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "#fff",
     alignItems: "center",
     justifyContent: "center",
+    borderBottomColor: Platform.OS === "ios" ? "#ccc" : "transparent",
+    borderBottomWidth: Platform.OS === "ios" ? 1 : 0,
   },
   headerTitle: {
     color: "#000",
