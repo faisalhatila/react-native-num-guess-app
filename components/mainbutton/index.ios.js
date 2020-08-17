a482ca521deeec0b801f706c144d5c1e068bde1a
faisalhatila <hatilas1994@gmail.com>
2020-08-17 05:17:05 -0700

    main button component, saperate files created for both different os

--- components/mainbutton/index.ios.js
+++ components/mainbutton/index.ios.js
@@ -1,36 +1,23 @@
 import React from "react";
 import {
-  View,
-  Button,
   StyleSheet,
   TouchableOpacity,
   Text,
-  Platform,
-  TouchableNativeFeedback,
 } from "react-native";
 import Colors from "../../constants/colors";
 
 const MainButton = (props) => {
-  let ButtonComponent = TouchableOpacity;
-  if (Platform.OS === "android" && Platform.Version >= 21) {
-    ButtonComponent = TouchableNativeFeedback;
   }
   return (
-    <View style={styles.buttonContainer}>
-      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+      <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
         <View style={styles.button}>
           <Text style={styles.buttonText}>{props.children}</Text>
         </View>
-      </ButtonComponent>
-    </View>
+      </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    borderRadius: 25,
-    overflow: "hidden",
-  },
   button: {
     backgroundColor: Colors.primaryColor,
     paddingVertical: 12,
